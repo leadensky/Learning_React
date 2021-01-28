486d20f154270f25e8eb16b80c9f2a7c0bf8471b
leadensky <ben@leadensky.com>
2021-01-28 08:24:49 -0700

    Chapter 6, page 114

--- recipes-app2/src/App.js
+++ recipes-app2/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import Menu from "./components/Menu";
-import data from "./data/recipes.json";
+// import Menu from "./components/Menu";
+// import data from "./data/recipes.json";
+import React, { useState } from "react";
+import colorData from "./data/color-data.json"
+import ColorList from "./components/ColorList.js";
 
 function App() {
+  const[colors, setColors] = useState(colorData);
   return (
-    <div className="App">
-      <Menu recipes={data} />
-    </div>
+    <ColorList 
+      colors={colors} 
+      onRemoveColor={id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+      onRateColor={(id, rating) => {
+        const newColors = colors.map(color => 
+          color.id === id ? {...color, rating } : color
+        );
+        setColors(newColors);
+      }}
+    />
   );
+  // return (
+  //   <div className="App">
+  //     <Menu recipes={data} />
+  //   </div>
+  // );
 }
 
 export default App;
--- recipes-app2/src/components/Recipe.js
+++ recipes-app2/src/components/Recipe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
+import StarRating from "./StarRating";
 
 function Recipe({ name, ingredients, steps }) {
     return (
@@ -8,6 +9,7 @@ function Recipe({ name, ingredients, steps }) {
             <h1>{name}</h1>
             <IngredientsList list={ingredients} />
             <Instructions title="Cooking Instructions" steps={steps} />
+            <StarRating />
         </section>
     );
 }
