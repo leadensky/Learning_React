55020f8452a7236ba585de832eb83e7772947c47
leadensky <ben@leadensky.com>
2021-01-28 07:24:08 -0700

    End of Chapter 5

--- recipes-app/index.js
+++ recipes-app/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Menu from "./components/Menu";
-import data from "./data/recipes.json";
+import Menu from "./src/components/Menu";
+import data from "./src/data/recipes.json";
 
 render(<Menu recipes={data} />, document.getElementById("root"));
